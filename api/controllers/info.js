c4d70427c5d9c96f0f16a308ade0d848100290e8
Haakam21 <haakamdeveloper@gmail.com>
2020-10-24 23:57:33 -0400

    Info stored in DB

--- api/controllers/info.js
+++ api/controllers/info.js
@@ -1,7 +1,9 @@
 const infoRouter = require('express').Router()
 
+const Info = require('../models/info')
+
 infoRouter.get('/', async (request, response) => {
-  const info = { videoURL: "https://www.youtube.com/embed/1aPPNK6nmck", text: "hello!" }
+  const info = await Info.findOne()//{ videoURL: "https://www.youtube.com/embed/1aPPNK6nmck", text: "Hello World!" }
 
   response.send(info)
 })
---
+++ api/models/info.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const infoSchema = new mongoose.Schema({
+  videoURL: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  date: Date
+})
+
+infoSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Info = mongoose.model('Info', infoSchema)
+
+module.exports = Info
